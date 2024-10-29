d91294ca220ab741cdd53a91da5885a928a1c0c4
ryan <exogenesis@protonmail.com>
2024-10-29 22:06:55 +1100

    feat: Add missing newline to tab content

--- src/processor.rs
+++ src/processor.rs
@@ -339,6 +339,7 @@ Tab content 3
 :::"#;
 
         let expected_output = r#"<div role="tablist" class="tabs tabs-lifted">
+
   <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 1" />
   <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
     Tab content 1
@@ -359,6 +360,7 @@ Tab content 3
   <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
     Tab content 3
   </div>
+
 </div>"#;
 
         let mut processor = Processor::default();
