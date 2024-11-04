61341ae8eb61d6dee16d22b0b2baee07a0d92b3b
ryan <exogenesis@protonmail.com>
2024-11-04 14:39:56 +1100

    refactor: Move `generate_ascii_diamond` function to a closure
    feat: move closure definition into registration
    
    fix: Replace asterisks with custom separator in diamond pattern
    
    feat: move separator variable into closure
    
    feat: Add custom string separator function for Rhai
    
    feat: combine separators for ASCII diamond generation
    
    better comments

--- src/bin/md_example_custom_functions.rs
+++ src/bin/md_example_custom_functions.rs
@@ -11,39 +11,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let test_string = fs::read_to_string(input_path)?;
 
     // Register custom functions
-    // Define test functions
-    fn generate_ascii_diamond(size: i64) -> String {
-        if size == 0 {
-            println!("Size must be greater than 0.");
-            return "".to_string();
-        }
-
-        let mut output = String::new();
-
-        // Upper part of the diamond including the middle line
-        for i in 0..size {
-            let spaces = " ".repeat((size - i) as usize);
-            let stars = "*".repeat((2 * i + 1) as usize);
-            let line = format!("{spaces}{stars}\n");
-            output.push_str(&line);
-        }
-
-        // Lower part of the diamond
-        for i in (0..size - 1).rev() {
-            let spaces = " ".repeat((size - i) as usize);
-            let stars = "*".repeat((2 * i + 1) as usize);
-            let line = format!("{spaces}{stars}\n");
-            output.push_str(&line);
-        }
-        format!("<pre>\n{}\n</pre>", output)
-    }
-
     fn double(x: i64) -> i64 {
         x * 2
     }
     fn concat(a: String, b: String) -> String {
         format!("{}{}", a, b)
     }
+    let separator = "¶"; // This will be cloned into the closure below
+    let sep2 = "$"; // The closure will take an immutable reference to this string
     let functions: Vec<CustomFn> = vec![
         Box::new(|engine: &mut Engine| {
             engine.register_fn("double", double);
@@ -51,8 +26,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         Box::new(|engine: &mut Engine| {
             engine.register_fn("concat", concat);
         }),
-        Box::new(|engine: &mut Engine| {
-            engine.register_fn("generate_ascii_diamond", generate_ascii_diamond);
+        Box::new(move |engine: &mut Engine| {
+            let separator = separator.to_string(); // Clone it here so we can move it into the next closure
+            engine.register_fn("generate_ascii_diamond", move |size: i64| -> String {
+                if size == 0 {
+                    println!("Size must be greater than 0.");
+                    return "".to_string();
+                }
+
+                let separator = format!("{separator}{sep2}");
+
+                let separator = format!("{separator}{sep2}");
+
+                let mut output = String::new();
+
+                // Upper part of the diamond including the middle line
+                for i in 0..size {
+                    let spaces = " ".repeat((size - i) as usize);
+                    let stars = separator.repeat((2 * i + 1) as usize);
+                    let line = format!("{spaces}{stars}\n");
+                    output.push_str(&line);
+                }
+
+                // Lower part of the diamond
+                for i in (0..size - 1).rev() {
+                    let spaces = " ".repeat((size - i) as usize);
+                    let stars = separator.repeat((2 * i + 1) as usize);
+                    let line = format!("{spaces}{stars}\n");
+                    output.push_str(&line);
+                }
+                format!("<pre>\n{}\n</pre>", output)
+            });
         }),
     ];
 
